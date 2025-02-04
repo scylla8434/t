bcd2b910b4bed693670e0ffd9ff3f3d4a5c764d2
scylla8434 <teddyamalemba@gmail.com>
2025-02-04 14:32:07 +0300

    vicky's

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+// login.php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="action.php" method="post">
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="login">Login</button>
+    </form>
+    <p>Don't have an account? <a href="signup.php">Sign Up here</a></p>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,25 @@
+<?php
+// signup.php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+</head>
+<body>
+    <h2>Sign Up</h2>
+    <form action="action.php" method="post">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="signup">Sign Up</button>
+    </form>
+    <p>Already have an account? <a href="login.php">Login here</a></p>
+</body>
+</html>
